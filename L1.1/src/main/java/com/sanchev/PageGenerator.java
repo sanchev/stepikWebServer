79e9d6530f458c0b0da572a9e9bd06a5bea9d769
sanchev <sanchev_temp@inbox.ru>
2016-11-07 12:07:49 +0200

    L1.1 Simple server

--- L1.1/src/main/java/com/sanchev/PageGenerator.java
+++ L1.1/src/main/java/com/sanchev/PageGenerator.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class PageGenerator {
 
-    private static final String HTML_DIR = "src/templates";
+    private static final String HTML_DIR = "templates";
 
     private static PageGenerator instance;
     private final Configuration configuration;
