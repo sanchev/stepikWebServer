02b61bd7a281fcd45c78f5c331c736a0e6a15dc9
sanchev <sanchev_temp@inbox.ru>
2017-01-20 16:23:00 +0200

    L6.1 Properties

---
+++ L6.1 Properties/src/main/java/com/sanchev/App.java
@@ -0,0 +1,22 @@
+package com.sanchev;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+
+            System.out.println(properties.getProperty("database"));
+            System.out.println(properties.getProperty("dbuser"));
+            System.out.println(properties.getProperty("dbpassword"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+ 	}
+}
