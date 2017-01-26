2953fffaf62d648a0105e28ba12c1f4b61780840
sanchev <sanchev_temp@inbox.ru>
2017-01-26 13:09:13 +0200

    Moved to HW03

--- HW03 (solution)/src/main/java/com/sanchev/base/AccountService.java
+++
@@ -1,6 +0,0 @@
-package com.sanchev.base;
-
-public interface AccountService {
-    void signUp(String login, String password);
-    boolean signIn(String login, String password);
-}
--- HW03 (solution)/src/main/java/com/sanchev/db/DBException.java
+++
@@ -1,7 +0,0 @@
-package com.sanchev.db;
-
-public class DBException extends Throwable{
-    public DBException(Throwable throwable) {
-        super(throwable);
-    }
-}
